18494f6748f9d8e27bb3dc9a4792f64f8fa56aa3
Abdulkareem <Abdulkareem@LA-abdulkareem.sssit.local>
2019-07-09 12:07:07 +0300

    test again

---
+++ Object Repository/Page_DashboardHome/i_  _la la-power-off.rs
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_  _la la-power-off</name>
+   <tag></tag>
+   <elementGuidId>7aa73eb9-281b-4dab-b5e5-0cc778a5293d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='تعريف ورقة الإحالة'])[1]/following::i[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>la la-power-off</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;sb-init loaded&quot;]/body[@class=&quot;vertical-layout vertical-menu 2-columns menu-flipped fixed-navbar menu-expanded&quot;]/div[@class=&quot;fixed-top&quot;]/div[@class=&quot;top-header&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;d-flex flex-column flex-sm-row&quot;]/div[@class=&quot;d-flex mr-sm-auto  mr-0 align-content-around flex-wrap h-side&quot;]/div[@class=&quot;d-flex&quot;]/div[@class=&quot;px-2 mr-3&quot;]/a[1]/i[@class=&quot;la la-power-off&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='تعريف ورقة الإحالة'])[1]/following::i[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='تفضيلات المستخدم'])[1]/following::i[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='خروج'])[1]/preceding::i[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Toggle navigation'])[1]/preceding::i[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div[2]/a/i</value>
+   </webElementXpaths>
+</WebElementEntity>
